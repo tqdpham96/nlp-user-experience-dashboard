86d3d82cf37c401c99fa74d233c035580e5f1736
Noisy <noisy.noah910@gmail.com>
2023-12-21 23:02:01 +0100

    feat: fix echarts

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ nltk.download('punkt')
 nltk.download('stopwords')
 nltk.download('wordnet')
 
+st.markdown(" <style>iframe{ height: 300px !important } ", unsafe_allow_html=True)
+
 st.set_page_config(
     page_title="Customer Experience Dashboard",
     page_icon="✅",
@@ -218,17 +220,14 @@ with tab_aspect:
     st.markdown("<h3 style='text-align: left;'>Customer's Voice</h3>", unsafe_allow_html=True)
     m1, m2 = st.columns(2)
     customerVoices = utils.getThreeWords(data[job_filter]['data'])
-    st.markdown("<h4 style='text-align: left;'>Positive</h4>", unsafe_allow_html=True)
-    posVoiceChart = ST_DEMOS["Bar: Basic Vertical Bar"]
-    posVoiceChart(customerVoices['pos'], customerVoices['posValue'], "Seriousness", 1, "customer_voice_positive")
-    # with m1:
-    #     st.markdown("<h4 style='text-align: left;'>Positive</h4>", unsafe_allow_html=True)
-    #     posVoiceChart = ST_DEMOS["Bar: Basic Vertical Bar"]
-    #     posVoiceChart(customerVoices['pos'], customerVoices['posValue'], "Seriousness", 1, "customer_voice_positive")
-    # with m2:
-    #     st.markdown("<h4 style='text-align: left;'>Negative</h4>", unsafe_allow_html=True)
-    #     # negVoiceChart = ST_DEMOS["Bar: Basic Vertical Bar"]
-    #     # negVoiceChart(customerVoices['neg'], customerVoices['negValue'], "Seriousness", 1, "customer_voice_negative")
+    with m1:
+        st.markdown("<h4 style='text-align: left;'>Positive</h4>", unsafe_allow_html=True)
+        posVoiceChart = ST_DEMOS["Bar: Basic Vertical Bar"]
+        posVoiceChart(customerVoices['pos'], customerVoices['posValue'], "Seriousness", 1, "customer_voice_positive")
+    with m2:
+        st.markdown("<h4 style='text-align: left;'>Negative</h4>", unsafe_allow_html=True)
+        negVoiceChart = ST_DEMOS["Bar: Basic Vertical Bar"]
+        negVoiceChart(customerVoices['neg'], customerVoices['negValue'], "Seriousness", 1, "customer_voice_negative")
 
     st.divider()
     st.markdown("<h3 style='text-align: left;'>Aspect Co-occurrence</h3>", unsafe_allow_html=True)
